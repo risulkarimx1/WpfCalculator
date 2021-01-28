1ba8f33635dbc52db3e407d607e9018777ac7435
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-28 11:45:33 +0100

    added commands for buttons

---
+++ IonCalculator.App/CalculatorModule/Helper/CalculatorStackHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CalculatorModule.Helper
+{
+    public class CalculatorStackHelper
+    {
+        public Stack<string> CalculatorStack { get; }
+
+        public CalculatorStackHelper()
+        {
+            CalculatorStack = new Stack<string>();
+        }
+
+        public void AddToStack(string input)
+        {
+            CalculatorStack.Push(input);
+        }
+    }
+}
--- IonCalculator.App/CalculatorModule/ViewModels/CalculatorViewModel.cs
+++ IonCalculator.App/CalculatorModule/ViewModels/CalculatorViewModel.cs
@@ -1,19 +1,44 @@
-﻿using Prism.Mvvm;
+﻿using System.Linq;
+using CalculatorModule.Helper;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace CalculatorModule.ViewModels
 {
     public class CalculatorViewModel : BindableBase
     {
+        private CalculatorStackHelper _stackHelper;
         private string _message;
+        private string _displayText;
+        public DelegateCommand<string> OnButtonPressed { get; set; }
+
+
+        public CalculatorViewModel()
+        {
+            OnButtonPressed = new DelegateCommand<string>(OnPressed);
+            _stackHelper = new CalculatorStackHelper();
+        }
+
+        public string DisplayText
+        {
+            get => _displayText;
+            set => SetProperty(ref _displayText, value);
+        }
+
+
+        private void OnPressed(string input)
+        {
+            if (input != "=")
+            {
+                DisplayText += input;
+                _stackHelper.AddToStack(input);
+            }
+        }
+
         public string Message
         {
             get { return _message; }
             set { SetProperty(ref _message, value); }
-        }
-
-        public CalculatorViewModel()
-        {
-            Message = "View A from your Prism Module c";
         }
     }
 }
