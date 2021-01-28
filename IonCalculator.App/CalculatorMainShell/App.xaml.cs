68bdbcf8d344b15e1e95e5c7ccb9f8ca12992dee
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-28 11:00:41 +0100

    Initial Project Setup

---
+++ IonCalculator.App/CalculatorMainShell/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CalculatorMainShell.Views;
+using Prism.Ioc;
+using System.Windows;
+using CalculatorModule;
+using Prism.Modularity;
+
+namespace CalculatorMainShell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<CalculatorModuleSetup>();
+        }
+    }
+}
---
+++ IonCalculator.App/CalculatorModule/CalculatorModuleSetup.cs
@@ -0,0 +1,27 @@
+﻿using CalculatorModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace CalculatorModule
+{
+    public class CalculatorModuleSetup : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+
+        public CalculatorModuleSetup(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(CalculatorView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
