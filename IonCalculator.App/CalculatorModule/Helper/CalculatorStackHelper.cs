3abeecbdb6150f6aa251f5379128e13483163e77
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-28 13:26:36 +0100

    Added Add Subtract functionality

--- IonCalculator.App/CalculatorModule/Helper/CalculatorStackHelper.cs
+++ IonCalculator.App/CalculatorModule/Helper/CalculatorStackHelper.cs
@@ -1,19 +1,118 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace CalculatorModule.Helper
 {
     public class CalculatorStackHelper
     {
-        public Stack<string> CalculatorStack { get; }
+        public Stack<string> _calculatorStack;
 
         public CalculatorStackHelper()
         {
-            CalculatorStack = new Stack<string>();
+            _calculatorStack = new Stack<string>();
+        }
+
+        public bool CanProcess()
+        {
+            if (_calculatorStack.Contains("+") || _calculatorStack.Contains("-") || _calculatorStack.Contains("*") || _calculatorStack.Contains("/"))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public void AddToStack(string input)
         {
-            CalculatorStack.Push(input);
+            if (IsOperator(input))
+            {
+                if(_calculatorStack.Count == 0) return;
+                
+                var stackPeek = _calculatorStack.Peek();
+                if (input == stackPeek)
+                {
+                    return;
+                }
+            }
+
+            _calculatorStack.Push(input);
+            
+            
+        }
+
+        public void ClearStack()
+        {
+            _calculatorStack.Clear();
+        }
+
+        public string ProcessStack()
+        {
+            var firstValue = _calculatorStack.Pop();
+            var ope = _calculatorStack.Pop();
+            var lastValue = _calculatorStack.Pop();
+            _calculatorStack.Clear();
+            // add last value
+
+            double first;
+            double last;
+            double output = 0;
+
+            Double.TryParse(firstValue, out first);
+            Double.TryParse(lastValue, out last);
+            
+            
+            if (ope == "+")
+            {
+                output = first + last;
+                _calculatorStack.Push(output.ToString(CultureInfo.InvariantCulture));
+            }
+            if (ope == "-")
+            {
+                output = last - first;
+                _calculatorStack.Push(output.ToString(CultureInfo.InvariantCulture));
+            }
+            
+            if (ope == "*")
+            {
+                output = last * first;
+                _calculatorStack.Push(output.ToString(CultureInfo.InvariantCulture));
+            }
+
+            if (ope == "/")
+            {
+                if (last == 0)
+                {
+                    _calculatorStack.Clear();
+                    return string.Empty;
+                }
+                else
+                {
+                    output = last / first;
+                    _calculatorStack.Push(output.ToString(CultureInfo.InvariantCulture));
+                }
+            }
+            
+            
+            return output.ToString(CultureInfo.InvariantCulture);
+            
+        }
+
+        public bool IsOperator(string input)
+        {
+            if (input == "+" || input == "-" || input == "/" || input == "*") return true;
+            return false;
+        }
+
+        private void PrintStack()
+        {
+            Console.WriteLine("======================");
+            foreach (var val in _calculatorStack)
+            {
+                MessageBox.Show(val);
+            }
         }
     }
 }
--- IonCalculator.App/CalculatorModule/ViewModels/CalculatorViewModel.cs
+++ IonCalculator.App/CalculatorModule/ViewModels/CalculatorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Text.RegularExpressions;
 using CalculatorModule.Helper;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -8,10 +9,17 @@ namespace CalculatorModule.ViewModels
     public class CalculatorViewModel : BindableBase
     {
         private CalculatorStackHelper _stackHelper;
-        private string _message;
         private string _displayText;
         public DelegateCommand<string> OnButtonPressed { get; set; }
+        private bool _clearDisplay = false;
+        private string _currentOperator;
 
+
+        public string CurrentOperator
+        {
+            get => _currentOperator;
+            set => SetProperty(ref _currentOperator, value);
+        }
 
         public CalculatorViewModel()
         {
@@ -25,20 +33,48 @@ namespace CalculatorModule.ViewModels
             set => SetProperty(ref _displayText, value);
         }
 
-
         private void OnPressed(string input)
         {
-            if (input != "=")
+            if (_clearDisplay)
+            {
+                DisplayText = String.Empty;
+                _clearDisplay = false;
+            }
+            
+            if (input == "Reset")
+            {
+                _stackHelper.ClearStack();
+            }
+
+            if (Regex.IsMatch(input, @"^\d+$") || input == ".")
             {
                 DisplayText += input;
-                _stackHelper.AddToStack(input);
             }
-        }
-
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
+            else if (_stackHelper.IsOperator(input))
+            {
+                CurrentOperator = input;
+                _stackHelper.AddToStack(DisplayText);
+                if (_stackHelper.CanProcess())
+                {
+                    DisplayText = _stackHelper.ProcessStack();
+                    _stackHelper.AddToStack(input);
+                    _clearDisplay = true;
+                }
+                else
+                {
+                    _stackHelper.AddToStack(DisplayText);
+                    _stackHelper.AddToStack(input);
+                    _clearDisplay = true;
+                }
+            }
+            
+            else if(input == "=")
+            {
+                // var processedData = _stackHelper.ProcessStack();
+                // DisplayText = processedData;
+                // _clearDisplay = true;
+                // _stackHelper.ClearStack();
+            }
         }
     }
 }
